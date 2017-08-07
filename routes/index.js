49cf1782404ec649ca4f9d86a61da28123bf4469
Hakumai <inside.rice164@gmail.com>
2017-08-08 01:39:40 +0900

    [update] add navigation bar to main page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ let router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
    if (req.isAuthenticated()) {
-      res.render('index-logged-in');
+      console.log(req.user._json);
+      res.render('index-logged-in', {
+         name: req.user._json.name,
+         screen_name: req.user._json.screen_name,
+         profile_image_url: req.user._json.profile_image_url
+      });
    } else {
       res.render('index');
    }
