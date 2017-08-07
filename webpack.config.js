db47162e328f7d86b55d66955183241e2692f296
Hakumai <inside.rice164@gmail.com>
2017-08-07 19:17:15 +0900

    [update] webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 module.exports = {
    // メインとなるJavaScriptファイル（エントリーポイント）
-   entry: './src/scripts/client-main.js',
+   entry: {
+      'active': './src/scripts/client-main-active.js',
+      'inactive': './src/scripts/client-main-inactive.js'
+   },
    // ファイルの出力設定
    output: {
       //  出力ファイルのディレクトリ名
       path: `${__dirname}/public/javascripts`,
       // 出力ファイル名
-      filename: 'bundle.js'
+      filename: 'bundle-[name].js'
+   },
+   module : {
+      // Loaderの設定
+      rules : [
+         // CSSファイルの読み込み
+         {
+            // 対象となるファイルの拡張子
+            test: /\.css/,
+            loaders: ['style-loader', 'css-loader']
+         },
+      ]
    }
 };
