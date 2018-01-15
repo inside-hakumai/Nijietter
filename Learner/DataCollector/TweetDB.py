cca9abf9386fa9e99a3d9a1c7fc4f0ec8aa500f4
Hakumai <inside.rice164@gmail.com>
2018-01-16 02:04:23 +0900

    Add LabelDB

--- Learner/DataCollector/TweetDB.py
+++ Learner/DataCollector/TweetDB.py
@@ -64,3 +64,37 @@ class ReactionDB:
             user = (post_id, time.strftime("%a %b %d %H:%M:%S %z %Y"), fav, retweet)
             c.execute(sql, user)
             conn.commit()
+
+
+class LabelDB:
+
+    def __init__(self, db_path=resource.get_collect_db_path()):
+        self.db_path = db_path
+
+        with closing(sqlite3.connect(self.db_path)) as conn:
+            c = conn.cursor()
+
+            # executeメソッドでSQL文を実行する
+            create_table = "CREATE TABLE IF NOT EXISTS labels (id VARCHAR PRIMARY KEY, label INTEGER DEFAULT -1)"
+
+            c.execute(create_table)
+            conn.commit()
+
+    def insert_label(self, post_id: str, label: int):
+        ret_val = None
+        with closing(sqlite3.connect(self.db_path)) as conn:
+            c = conn.cursor()
+
+            sql = 'INSERT OR REPLACE INTO labels (id, label) values (?,?)'
+            data = (post_id, label)
+
+            try:
+                c.execute(sql, data)
+                conn.commit()
+            except:
+                import traceback
+                traceback.print_exc()
+                ret_val = False
+
+        ret_val = True
+        return ret_val
